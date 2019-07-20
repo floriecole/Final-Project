812865db6af3359ed44857789616c2f252d285e6
floriecole <floriejrcole@gmail.com>
2019-07-20 12:22:44 -0400

    upload files

--- clean_books.sql
+++ clean_books.sql
@@ -3,4 +3,22 @@ DROP DATABASE IF EXISTS books_db;
 
 CREATE DATABASE books_db;
 USE books_db;
-select * from books_data
+select * from books_data;
+
+-- TABLE IF EXISTS books_data;
+
+-- CREATE TABLE books_data (
+    -- Id int NOT NULL AUTO_INCREMENT,
+    -- ISBN varchar(255) NOT NULL,
+    -- Title varchar(255),
+    -- Author varchar(255),
+    -- Year-Of-Publication int,
+   -- Publisher varchar(255),
+    -- image_url varchar(255),
+    -- book_id int, 
+    -- genres varchar(255),
+    -- User-ID int,
+   -- Book-Rating decimal,
+   -- birthplace varchar(255),
+   -- PRIMARY KEY (Id)
+);
---
+++ knn_data.sql
@@ -0,0 +1,6 @@
+-- Create and use book_db
+DROP DATABASE IF EXISTS knn_db;
+
+CREATE DATABASE knn_db;
+USE knn_db;
+select * from knn_data;
